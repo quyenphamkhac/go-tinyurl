1f3ec5f1cf705c70b719ea73a29749b56674a342
Quyen Pham Khac <pkquyen@sk-global.biz>
2021-03-22 10:33:16 +0700

    fix(count): fixing count user problem

--- repos/user_repo.go
+++ repos/user_repo.go
@@ -24,7 +24,7 @@ func NewUserRepository(s *gocql.Session) *UserRepository {
 func (r *UserRepository) CreateUser(userDto *dtos.SignUpDto) (*entities.User, error) {
 	var user *entities.User
 	var count int
-	r.session.Query("SELECT COUNT(*) FROM users WHERER username = ? ALLOW FILTERING", userDto.Username).Iter().Scan(&count)
+	r.session.Query("SELECT COUNT(*) FROM users WHERE username = ? ALLOW FILTERING", userDto.Username).Iter().Scan(&count)
 	if count > 0 {
 		return user, errors.New("user already exists")
 	}
@@ -39,10 +39,11 @@ func (r *UserRepository) CreateUser(userDto *dtos.SignUpDto) (*entities.User, er
 		Email:          userDto.Email,
 		ID:             gocql.TimeUUID().String(),
 		CreationDate:   time.Now(),
+		LastLogin:      time.Now(),
 	}
 	ctx := context.Background()
-	if err := r.session.Query(`INSERT INTO users (username, name, hashed_password, email, id, creation_date) VALUES (?, ?, ?, ?, ?, ?)`,
-		user.Username, user.Name, user.HashedPassword, user.Email, user.ID, user.CreationDate).WithContext(ctx).Exec(); err != nil {
+	if err := r.session.Query(`INSERT INTO users (username, name, hashed_password, email, id, creation_date, last_login) VALUES (?, ?, ?, ?, ?, ?, ?)`,
+		user.Username, user.Name, user.HashedPassword, user.Email, user.ID, user.CreationDate, user.LastLogin).WithContext(ctx).Exec(); err != nil {
 		return nil, err
 	}
 	return user, nil
