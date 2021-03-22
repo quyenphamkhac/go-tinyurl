c58d95931e833fa7bfbd33e1b91ab1cca038d0f4
Quyen Pham Khac <pkquyen@sk-global.biz>
2021-03-22 21:13:25 +0700

    fix: abort when verify error fail

--- middlewares/auth_middleware.go
+++ middlewares/auth_middleware.go
@@ -2,26 +2,38 @@ package middlewares
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/quyenphamkhac/go-tinyurl/services"
 )
 
+type authorizeHeader struct {
+	Token string `header:"Authorization"`
+}
+
 func AuthorizeWithJwt(jwtService *services.JwtService) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		authHeader := c.GetHeader("Authorization")
-		if authHeader == "" {
-			c.JSON(http.StatusUnauthorized, gin.H{
+		var h authorizeHeader
+		if err := c.ShouldBindHeader(&h); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+		}
+		token := strings.Split(h.Token, "Bearer ")
+		if len(token) < 2 {
+			c.JSON(http.StatusBadRequest, gin.H{
 				"error": "Must provide Authorization header with format `Bearer {token}`",
 			})
+			c.Abort()
 			return
 		}
-		token := authHeader[len("Bearer")+1:]
-		claims, err := jwtService.VerifyToken(token)
+		claims, err := jwtService.VerifyToken(token[1])
 		if err != nil {
 			c.JSON(http.StatusUnauthorized, gin.H{
 				"error": err.Error(),
 			})
+			c.Abort()
 			return
 		}
 		c.Set("user", claims.User)
