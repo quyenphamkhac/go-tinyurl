d4051f715f6cb7276439de8d621c461966dd199b
Quyen Pham Khac <pkquyen@sk-global.biz>
2021-03-23 21:27:39 +0700

    feat: using cacheeeee

--- repos/cache_repo.go
+++ repos/cache_repo.go
@@ -1,6 +1,10 @@
 package repos
 
 import (
+	"context"
+	"fmt"
+	"time"
+
 	"github.com/go-redis/cache/v8"
 	"github.com/quyenphamkhac/go-tinyurl/entities"
 )
@@ -16,8 +20,24 @@ func NewCacheRepository(c *cache.Cache) *CacheRepository {
 }
 
 func (c *CacheRepository) SetURL(url *entities.URL) {
+	ctx := context.TODO()
+	if err := c.cache.Set(&cache.Item{
+		Ctx:   ctx,
+		Key:   url.Hash,
+		Value: url,
+		TTL:   time.Hour,
+	}); err != nil {
+		fmt.Println("[Error] Redis set URL fails: ", err.Error())
+	}
 }
 
 func (c *CacheRepository) GetURL(hash string) *entities.URL {
-	return nil
+	var url entities.URL
+	ctx := context.TODO()
+	err := c.cache.Get(ctx, hash, &url)
+	if err != nil {
+		fmt.Println("[Error] Redis get URL fails: ", err.Error())
+		return nil
+	}
+	return &url
 }
