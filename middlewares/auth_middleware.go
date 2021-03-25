4ea1393daaea3f8ef0e1144d8bdc8c7b9a88aab5
Quyen Pham Khac <pkquyen@sk-global.biz>
2021-03-25 14:30:05 +0700

    fix: abort with error instead just error

--- middlewares/auth_middleware.go
+++ middlewares/auth_middleware.go
@@ -17,17 +17,17 @@ func AuthorizeWithJwt(jwtService *services.JwtService) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var h authorizeHeader
 		if err := c.ShouldBindHeader(&h); err != nil {
-			c.Error(apperrors.New(http.StatusBadRequest, err.Error()))
+			c.AbortWithError(http.StatusBadRequest, apperrors.New(http.StatusBadRequest, err.Error()))
 			return
 		}
 		token := strings.Split(h.Token, "Bearer ")
 		if len(token) < 2 {
-			c.Error(apperrors.New(http.StatusUnauthorized, "Must provide Authorization header with format `Bearer {token}`"))
+			c.AbortWithError(http.StatusUnauthorized, apperrors.New(http.StatusUnauthorized, "Must provide Authorization header with format `Bearer {token}`"))
 			return
 		}
 		claims, err := jwtService.VerifyToken(token[1])
 		if err != nil {
-			c.Error(apperrors.New(http.StatusUnauthorized, err.Error()))
+			c.AbortWithError(http.StatusUnauthorized, apperrors.New(http.StatusUnauthorized, err.Error()))
 			return
 		}
 		c.Set("user", claims.User)
